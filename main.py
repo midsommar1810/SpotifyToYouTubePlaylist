371b4f4eb11aadfcefb86bd191c1f5ddc17609ee
midsommar1810 <abhishekraipur564@gmail.com>
2024-11-01 20:13:36 +0530

    Modified cliet ID and client secret of spotify

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ class SpotifyYouTubeConverter:
     def __init__(self):
         # Spotify Authentication
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id="your_spotify_client_id",
-            client_secret="your_spotify_client_secret",
-            redirect_uri="your_redirect_uri",
+            client_id="0cdb670e82a943e6b810c5ccd2cf8b9f",
+            client_secret="4f6e8d8f791a43049c0efa7b5cf6057b",
+            redirect_uri="http://localhost:8888/callback",  # Make sure this matches your registered URI
             scope="playlist-read-private"))
 
         # YouTube Authentication
+        self.youtube = self.authenticate_youtube()
+
+    def authenticate_youtube(self):
+        # Set up YouTube authentication
         flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", ["https://www.googleapis.com/auth/youtube"])
-        self.credentials = flow.run_local_server(port=0)
-        self.youtube = build("youtube", "v3", credentials=self.credentials)
+        credentials = flow.run_local_server(port=0)
+        return build("youtube", "v3", credentials=credentials)
 
     def convert_playlist(self, playlist_url):
         playlist_id = playlist_url.split("/")[-1].split("?")[0]
@@ -76,4 +80,5 @@ class SpotifyYouTubeConverter:
 
 if __name__ == "__main__":
     converter = SpotifyYouTubeConverter()
-    # You can call any method directly here for testing, if needed.
+    # Uncomment below for testing purposes if needed
+    # converter.convert_playlist("your_spotify_playlist_url")
